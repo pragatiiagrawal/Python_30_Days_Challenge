cd2c88a5430e67bbe7710dc52bd475e48820ecb4
Pragati Agrawal <agrawalpragati25@gmail.com>
2025-05-28 23:53:39 +0530

    day_2.py

--- DailyChallenges/day_2.py
+++ DailyChallenges/day_2.py
@@ -2,15 +2,12 @@
 # Write a program that calculates the area of a rectangle using user-input length and width.
 
 print("----Let's Calculate the area of rectangle ----")
-
 # Step-1 Taking input from the user
 # Step-2 The input function returns a string, so we use int() to convert it to an integer
-
 length = int(input("Enter the length of the rectangle in cm: "))
 width = int(input("Enter the width of the rectangle in cm: "))
-
 # Calculating area
-
 area = length * width
+# f-string allows us to embedd expressions directly in the string.
 # \u00B2 is the Unicode for superscript 2 
 print(f"The area of the rectangle is {area} cm\u00B2.")
