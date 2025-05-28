0a268530e05dee270b580e3474008d51a9035fac
Pragati Agrawal <agrawalpragati25@gmail.com>
2025-05-28 13:49:50 +0530

    day_1.py

--- DailyChallenges/day_1.py
+++ DailyChallenges/day_1.py
@@ -1,2 +1,5 @@
+#Day 1 Python_IDC_Challenge
+# Write a program that prints "Hello, World!" followed by your name
+
 my_name = input("Please enter your name:")
-print("Hello World,", my_name)
+print(f'Hello, World!,{my_name},kicking off the journey.')
