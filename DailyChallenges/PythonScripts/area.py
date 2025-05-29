f79a775514b0190edb0eae06c1a8164aae38ab11
Pragati Agrawal <agrawalpragati25@gmail.com>
2025-05-29 13:02:31 +0530

    area.ipynb

--- DailyChallenges/PythonScripts/area.py
+++ DailyChallenges/PythonScripts/area.py
@@ -4,10 +4,11 @@
 print("----Let's Calculate the area of rectangle ----")
 # Step-1 Taking input from the user
 # Step-2 The input function returns a string, so we use int() to convert it to an integer
-length = int(input("Enter the length of the rectangle in cm: "))
-width = int(input("Enter the width of the rectangle in cm: "))
+length = float(input("Enter the length of the rectangle in cm: "))
+width = float(input("Enter the width of the rectangle in cm: "))
 # Calculating area
 area = length * width
 # f-string allows us to embedd expressions directly in the string.
 # \u00B2 is the Unicode for superscript 2 
-print(f"The area of the rectangle is {area} cm\u00B2.")
+# .2f rounds the float to 2 decimal places.
+print(f"The area of the rectangle is {area:.2f} cm\u00B2.")
